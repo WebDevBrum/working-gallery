92411e7199e54f02cd5c122891a247045243ca43
Scott Davies <scottdavies1983@gmail.com>
2020-12-01 15:14:39 +0000

    initial set up of click and modal

--- gallery.js
+++ gallery.js
@@ -1,1 +1,29 @@
-console.log(test);
+function Gallery(gallery) {
+  if (!gallery) {
+    throw new Error("No Gallery Found!");
+  }
+  // select the elemmt we need
+  const images = Array.from(gallery.querySelectorAll("img"));
+  const modal = document.querySelector(".modal");
+  const prevButton = modal.querySelector(".prev");
+  const nextButton = modal.querySelector(".next");
+
+  function showImage(el) {
+    if (!el) {
+      console.info("no image to show");
+      return;
+    }
+    // update modal with this info
+    console.log(el);
+  }
+  function handleImageClick(event) {
+    showImage(event.currentTarget);
+  }
+
+  images.forEach((image) => image.addEventListener("click", handleImageClick));
+}
+
+// Use it on the page
+
+const gallery1 = Gallery(document.querySelector(".gallery1"));
+const gallery2 = Gallery(document.querySelector(".gallery2"));
