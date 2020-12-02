c51d88c94585fb3cecda1ab5f9ef97381abf6b32
Scott Davies <scottdavies1983@gmail.com>
2020-12-02 05:43:41 +0000

    modal window functionality in progress

--- gallery.js
+++ gallery.js
@@ -7,6 +7,41 @@ function Gallery(gallery) {
   const modal = document.querySelector(".modal");
   const prevButton = modal.querySelector(".prev");
   const nextButton = modal.querySelector(".next");
+  let currentImage;
+
+  function openModal() {
+    console.info("Opening Modal...");
+    // First check if the modal is already open
+    if (modal.matches("open")) {
+      console.info("Modal already open");
+      // stop the function from running
+    }
+    modal.classList.add("open");
+    // Event listeners to be bound when we open the modal
+    window.addEventListener("keyup", handleKeyUp);
+    nextButton.addEventListener("click", showNextImage);
+  }
+
+  function closeModal() {
+    modal.classList.remove("open");
+    // TODO add event listeners for clicks and keyboard..
+    window.removeEventListener("keyup", handleKeyUp);
+    nextButton.removeEventListener("click", showNextImage);
+  }
+
+  function handleClickOutside(e) {
+    if (e.target === e.currentTarget) {
+      closeModal();
+    }
+  }
+
+  function handleKeyUp(event) {
+    if (event.key === "Escape") closeModal();
+  }
+
+  function showNextImage() {
+    console.log(currentImage.nextElementSibling);
+  }
 
   function showImage(el) {
     if (!el) {
@@ -15,12 +50,18 @@ function Gallery(gallery) {
     }
     // update modal with this info
     console.log(el);
-  }
-  function handleImageClick(event) {
-    showImage(event.currentTarget);
+    modal.querySelector("img").src = el.src;
+    modal.querySelector("h2").textContent = el.title;
+    modal.querySelector("figure p").textContent = el.dataset.description;
+    currentImage = el;
+    openModal();
   }
 
-  images.forEach((image) => image.addEventListener("click", handleImageClick));
+  // these are event listeners
+  images.forEach((image) =>
+    image.addEventListener("click", (e) => showImage(e.currentTarget))
+  );
+  modal.addEventListener("click", handleClickOutside);
 }
 
 // Use it on the page
